8b5789ad4e5af68ffdc22d973ad8c8e1be7a756c
James Shadrake <jimkarts@hotmail.co.uk>
2019-03-17 23:38:21 +0000

    run.py and css updated for new login and sign up pages

--- run.py
+++ run.py
@@ -8,6 +8,22 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
     
+    
+
+"""Rendering the signup page and dealing with the form"""
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
+
+"""Rendering the login page and dealing with the form"""
+@app.route("/login")
+def login():
+    return render_template("login.html")
+    
+    
+    
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
     port=int(os.environ.get("PORT")),
