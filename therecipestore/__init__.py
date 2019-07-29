30a90040d731c5847555c16398b5d0a16eb45125
James Shadrake <james.shadrake@gmail.com>
2019-07-29 15:49:00 +0000

    Updated __init__ to check db

--- therecipestore/__init__.py
+++ therecipestore/__init__.py
@@ -7,7 +7,7 @@ import os
 username = os.getenv('C9_USER')
 app = Flask(__name__)
 app.secret_key = "SECRET KEY"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://yubehjsptoqvok:b0c1593f028ea0af4c8d2b656f070b3a80e55b36c97831a847b9f6a507795644@ec2-54-75-230-41.eu-west-1.compute.amazonaws.com:5432/d340c3tp066fuc'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///the_recipe_store.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
